507f02a8327d35850844b2751ef61ce66c3ed71b
antoanLengerdjiev <alengerdjiev@gmail.com>
2016-11-29 16:54:20 +0200

    unnecessary argument removed

--- server/config/routers/index.js
+++ server/config/routers/index.js
@@ -3,10 +3,10 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = function(app, data) {
+module.exports = function(app) {
     fs.readdirSync("./server/config/routers")
         .filter(x => x.includes("-router"))
         .forEach(file => {
-            require(path.join(__dirname, file))(app, data);
+            require(path.join(__dirname, file))(app);
         });
 };
